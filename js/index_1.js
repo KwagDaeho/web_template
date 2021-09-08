85765081136c8ecc8a367f7cf86978bfbda0473d
Haraho <1438eogh@gmail.com>
2021-09-08 21:36:41 +0900

    add:scroll animation and images

--- js/index_1.js
+++ js/index_1.js
@@ -48,4 +48,24 @@ $(function () {
     }
   }
   list_06_copy();
+
+  // Scroll Animation (sa, 스크롤 애니메이션)
+  const saTriggerMargin = 300;
+  const saElementList = document.querySelectorAll(".sa");
+
+  const saFunc = function () {
+    for (const element of saElementList) {
+      if (!element.classList.contains("show")) {
+        if (
+          window.innerHeight >
+          element.getBoundingClientRect().top + saTriggerMargin
+        ) {
+          element.classList.add("show");
+        }
+      }
+    }
+  };
+
+  window.addEventListener("load", saFunc);
+  window.addEventListener("scroll", saFunc);
 });
