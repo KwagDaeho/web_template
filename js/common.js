fb31f142b9086f42e27b43e8af0515f869c713bd
Haraho <1438eogh@gmail.com>
2021-10-19 09:48:36 +0900

    modify

--- js/common.js
+++ js/common.js
@@ -3,4 +3,19 @@ $(function () {
   $(".modal_btn").click(function () {
     $(".modal_window").fadeToggle();
   });
+  $(".asd").focus();
 });
+
+for (var i = 0; i < $modal.length; i++) {
+  if ($modal[i].classList.contains("autoFocus")) {
+    $modal[i].focus();
+  }
+}
+
+var test = document.querySelectorAll(".thumb_box");
+console.log(test[0]);
+if (test[0].classList.contains("_NM_NEWSSTAND_THUMB")) {
+  console.log("클래스 확인 완료 ");
+  document.getElementById("query").focus();
+}
+console.log("길이 = " + test.length);
